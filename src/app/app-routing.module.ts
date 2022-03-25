d7c3bee84c4aee869bcd912159a8c71d905ee865
Niclas-Gustafsson <niclas.gustafsson@chasacademy.se>
2022-03-25 16:32:25 +0100

    Add home component to public

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrivateComponent } from './private/private.component';
+import { HomeComponent } from './public/home/home.component';
 import {PublicComponent} from "./public/public.component";
 
 const routes: Routes = [
-  {path: 'public', component: PublicComponent},
+  {
+    path: '',
+    component: PublicComponent,
+    children: [
+      {path: '', component: HomeComponent},
+    ]
+  },
+
+  {path: 'private', component: PrivateComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PublicComponent } from './public/public.component';
 import { PrivateComponent } from './private/private.component';
+import { HomeComponent } from './public/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PublicComponent,
-    PrivateComponent
+    PrivateComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
