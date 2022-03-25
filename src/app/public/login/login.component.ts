138f5b5d1330ebf13deb603d6e883cc74475c8dc
Niclas-Gustafsson <niclas.gustafsson@chasacademy.se>
2022-03-25 21:52:46 +0100

    Add public service to send http requests for users, write login and logout function.

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup } from '@angular/forms';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {PublicService} from "../public.service";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  constructor(private fb: FormBuilder, private router: Router, public publicService: PublicService) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      email: '',
+      password: ''
+    });
+  }
+  submit() {
+    const formData = this.form.getRawValue();
+    const data = {
+      email: formData.email,
+      password: formData.password,
+    }
+    this.publicService.login().subscribe((res: any) => {
+      localStorage.setItem('token', res.data.token);
+      this.router.navigate(['/private']);
+    });
+    //this.http.post('http://127.0.0.1:8000/api/login', data).subscribe(
+    //  (result:any) => {
+    //    //console.log('result');
+    //    //console.log(result.data.token);
+    //    localStorage.setItem('token', result.data.token);
+    //    this.router.navigate(['/private']);
+     // },
+    //  error => {
+    //    console.log('error');
+    //    console.log(error)
+    //  }
+    //);
   }
 
 }
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -5,6 +5,9 @@ import {PublicComponent} from "./public.component";
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import {RouterModule} from "@angular/router";
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http";
+
 
 
 
@@ -17,7 +20,9 @@ import {RouterModule} from "@angular/router";
   imports: [
     CommonModule,
     //AppRoutingModule
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class PublicModule { }
